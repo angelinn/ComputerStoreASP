e4297f19f0fc9fcb6cd656201a05359d8d0e9093
betrakiss <angelin.nedelchev@outlook.com>
2016-06-08 17:02:47 +0300

    Adds full data capturing from input.

--- src/XMLManager`/XMLWorker/ManualInput.aspx.cs
+++ src/XMLManager`/XMLWorker/ManualInput.aspx.cs
@@ -40,6 +40,62 @@ namespace XMLWorker
                 IntegratedVideo = chkCpuVideo.Checked
             };
 
+            store.Parts.VideoCards = new VideoCard[1];
+            store.Parts.VideoCards[0] = new VideoCard
+            {
+                Available = Int32.Parse(txtGpuAvailable.Text),
+                Bandwidth = txtGpuBandwidth.Text,
+                BusWidth = txtGpuBus.Text,
+                DirectX = Int32.Parse(txtGpuDirectX.Text),
+                GPUMemory = new GPUMemory { Amount = txtGpuMemoryAmount.Text, Type = txtGpuMemoryTypeID.Text },
+                ID = txtGpuID.Text,
+                Interface = txtGpuIface.Text,
+                Manufacturer = txtGpuManufacturer.Text,
+                Model = txtGpuModel.Text,
+                Price = txtGpuPrice.Text,
+                Shaders = txtGpuShaders.Text
+            };
+
+            store.Parts.RamBoards = new RamBoard[1];
+            store.Parts.RamBoards[0] = new RamBoard
+            {
+                Available = Int32.Parse(txtRamAvailable.Text),
+                Channel = drpRamChannel.SelectedValue,
+                Frequency = txtRamFrequency.Text,
+                ID = txtRamID.Text,
+                Manufacturer = txtRamManufacturer.Text,
+                Memory = txtRamMemory.Text,
+                Price = txtRamPrice.Text,
+                Type = txtRamType.Text
+            };
+
+            store.Parts.HardDrives = new HardDrive[1];
+            store.Parts.HardDrives[0] = new HardDrive
+            {
+                Available = Int32.Parse(txtHddAvailable.Text),
+                Bus = txtHddBus.Text,
+                DriveMemory = new DriveMemory { Amount = txtHddMemory.Text, Type = txtHddType.Text },
+                ID = txtHddID.Text,
+                LaptopCompatible = Boolean.Parse(txtHddLaptop.Text),
+                Manufacturer = txtHddManufacturer.Text,
+                Price = txtHddPrice.Text,
+                Size = txtHddSize.Text,
+                Speed = txtHddSpeed.Text
+            };
+
+            store.Sockets = new Socket[1];
+            store.Sockets[0] = new Socket
+            {
+                ID = txtSocketID.Text,
+                SocketName = txtSocketName.Text
+            };
+
+            store.MemoryTypes = new Memory[1];
+            store.MemoryTypes[0] = new Memory
+            {
+                ID = txtMemoryID.Text,
+                MemoryName = txtMemoryName.Text
+            };
 
             XmlSerializer serializer = new XmlSerializer(typeof(ComputerStore));
             XmlWriterSettings writerSettings = new XmlWriterSettings();
