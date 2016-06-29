798887e0ab520412fb5d1d6b37baf1862338b8d0
betrakiss <angelin.nedelchev@outlook.com>
2016-06-29 17:38:01 +0300

    Adds logic to save XML file from manual input and convert it to database. Not tested.

--- src/XMLManager`/XMLWorker/ManualInput.aspx.cs
+++ src/XMLManager`/XMLWorker/ManualInput.aspx.cs
@@ -11,6 +11,7 @@ using System.Xml;
 using System.Xml.Serialization;
 using XMLWorker.Common;
 using DataAccess.Repositories;
+using DataAccess.Domain;
 
 namespace XMLWorker
 {
@@ -48,98 +49,34 @@ namespace XMLWorker
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
             ComputerStore store = new ComputerStore();
-            
-            //store.Parts.Processors[0] = new Processor
-            //{
-            //  //  ID = txtCpuID.Text,
-            //    //Socket = txtCpuSocket.Text,
-            //    //Model = txtCpuModel.Text,
-            //    Manufacturer = txtCpuManufacturer.Text,
-            //    Architecture = txtCpuArchitecture.Text,
-            //    ClockFrequency = txtCpuFrequency.Text,
-            //    Cache = new Cache { Levels = Int32.Parse(txtCacheLevels.Text), Memory = txtCacheMemory.Text },
-            //    Threads = new Threads { Logical = Int32.Parse(txtLogicalThreads.Text), Physical = Int32.Parse(txtPhysicalThreads.Text) },
-            //    Available = Int32.Parse(txtCpuAvailable.Text),
-            //    Price = txtCpuPrice.Text,
-            //    IntegratedVideo = chkCpuVideo.Checked
-            //};
+            store.Parts = new Parts();
 
-            //store.Parts.VideoCards = new VideoCard[1];
-            //store.Parts.VideoCards[0] = new VideoCard
-            //{
-            //    Available = Int32.Parse(txtGpuAvailable.Text),
-            //    Bandwidth = txtGpuBandwidth.Text,
-            //    BusWidth = txtGpuBus.Text,
-            //    DirectX = Int32.Parse(txtGpuDirectX.Text),
-            //    GPUMemory = new GPUMemory { Amount = txtGpuMemoryAmount.Text, Type = txtGpuMemoryTypeID.Text },
-            //    ID = txtGpuID.Text,
-            //    Interface = txtGpuIface.Text,
-            //    Manufacturer = txtGpuManufacturer.Text,
-            //    Model = txtGpuModel.Text,
-            //    Price = txtGpuPrice.Text,
-            //    Shaders = txtGpuShaders.Text
-            //};
+            store.Parts.Processors = processors.ToArray();
+            store.Parts.VideoCards = videoCards.ToArray();
+            store.Parts.RamBoards = ramBoards.ToArray();
+            store.Parts.HardDrives = hardDrives.ToArray();
+            store.Parts.Motherboards = motherboards.ToArray();
+            store.Sockets = sockets.ToArray();
+            store.MemoryTypes = memoryTypes.ToArray();
 
-            //store.Parts.RamBoards = new RamBoard[1];
-            //store.Parts.RamBoards[0] = new RamBoard
-            //{
-            //    Available = Int32.Parse(txtRamAvailable.Text),
-            //    Channel = drpRamChannel.SelectedValue,
-            //    Frequency = txtRamFrequency.Text,
-            //    ID = txtRamID.Text,
-            //    Manufacturer = txtRamManufacturer.Text,
-            //    Memory = txtRamMemory.Text,
-            //    Price = txtRamPrice.Text,
-            //    Type = txtRamType.Text
-            //};
+            XmlSerializer serializer = new XmlSerializer(typeof(ComputerStore));
+            XmlWriterSettings writerSettings = new XmlWriterSettings();
+            writerSettings.Indent = true;
+            writerSettings.IndentChars = "\t";
 
-            //store.Parts.HardDrives = new HardDrive[1];
-            //store.Parts.HardDrives[0] = new HardDrive
-            //{
-            //    Available = Int32.Parse(txtHddAvailable.Text),
-            //    Bus = txtHddBus.Text,
-            //    DriveMemory = new DriveMemory { Amount = txtHddMemory.Text, Type = txtHddType.Text },
-            //    ID = txtHddID.Text,
-            //    LaptopCompatible = Boolean.Parse(txtHddLaptop.Text),
-            //    Manufacturer = txtHddManufacturer.Text,
-            //    Price = txtHddPrice.Text,
-            //    Size = txtHddSize.Text,
-            //    Speed = txtHddSpeed.Text
-            //};
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
 
-            //store.Sockets = new Socket[1];
-            //store.Sockets[0] = new Socket
-            //{
-            //    ID = txtSocketID.Text,
-            //    SocketName = txtSocketName.Text
-            //};
+            string resultXml = "C:\\Users\\betra\\desktop\\res.xml";
 
-            //store.MemoryTypes = new Memory[1];
-            //store.MemoryTypes[0] = new Memory
-            //{
-            //    ID = txtMemoryID.Text,
-            //    MemoryName = txtMemoryName.Text
-            //};
+            using (TextWriter textWriter = new StreamWriter(resultXml))
+            using (XmlWriter xmlWriter = XmlWriter.Create(textWriter, writerSettings))
+            {
+                xmlWriter.WriteDocType("computer-store", null, "computer_store.dtd", null);
+                serializer.Serialize(xmlWriter, store, ns);
+            }
 
-            //XmlSerializer serializer = new XmlSerializer(typeof(ComputerStore));
-            //XmlWriterSettings writerSettings = new XmlWriterSettings();
-            //writerSettings.Indent = true;
-            //writerSettings.IndentChars = "\t";
-
-            //XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
-            //ns.Add("", "");
-
-            //using (TextWriter textWriter = new StreamWriter("C:\\Users\\betra\\desktop\\res.xml"))
-            //using (XmlWriter xmlWriter = XmlWriter.Create(textWriter, writerSettings))
-            //{
-            //    xmlWriter.WriteDocType("computer-store", null, "computer_store.dtd", null);
-            //    serializer.Serialize(xmlWriter, store, ns);
-            //}
-        }
-
-        protected void btnInputCount_Click(object sender, EventArgs e)
-        {
-
+            ComputerStoreDO.AddStoreData(resultXml);
         }
 
         public IQueryable<Processor> GetProcessors()
